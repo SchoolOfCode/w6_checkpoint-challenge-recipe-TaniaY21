8dd476b5ebf24d919a13697d0fe73821a8218381
TaniaY21 <taniay@hotmail.co.uk>
2021-05-14 18:08:03 +0100

    refactored code

--- main.js
+++ main.js
@@ -15,75 +15,74 @@ async function fetchRecipe(food) {
 
 function handleRecipeClick() {
   foodToSearch = document.querySelector("#food-input").value;
-
-  fetchRecipe(foodToSearch).then((data) => {
-    // creates another function with parameter (data) to run AFTER await fetch is complete on line 13
-    let resultsFound = document.getElementById("resultsFound"); // span created to list result count
-    resultsFound.innerHTML = data.count;
-
-    let hits = data.hits;
-    const recipe_section = document.getElementById("recipe-section");
-    recipe_section.innerHTML = "";
-
-    for (let i = 0; i < 10; i++) {
-      // main div added to body
-      const cardDiv = document.createElement("div");
-      cardDiv.classList.add("flex-card-div");
-      recipe_section.appendChild(cardDiv);
-
-      const anchorText = document.createElement("a");
-      anchorText.id = "recipe-link";
-      anchorText.href = hits[i].recipe.url;
-      anchorText.text = hits[i].recipe.label;
-
-      const image = document.createElement("img");
-      image.classList.add("recipe-image");
-      image.src = hits[i].recipe.image;
-
-      // // child div for image
-      const imageDiv = document.createElement("div");
-      imageDiv.classList.add("flex-list-items");
-
-      const infoDiv = document.createElement("div");
-      infoDiv.classList.add("food-info-div");
-      const calorie_span = document.createElement("span");
-      calorie_span.classList.add("food-info-span");
-      calorie_span.innerHTML = `Calories: ${Math.floor(
-        hits[i].recipe.calories
-      )}`;
-
-      const serving_span = document.createElement("span");
-      serving_span.classList.add("food-info-span");
-      serving_span.innerHTML = `Serves: ${hits[i].recipe.yield}`;
-
-      const time_span = document.createElement("span");
-      time_span.classList.add("food-info-span");
-      let cookingTime = hits[i].recipe.totalTime
-        ? `${hits[i].recipe.totalTime} mins`
-        : "NA";
-      time_span.innerHTML = `Total Time: ${cookingTime}`;
-
-      const ul = document.createElement("ul");
-      ul.classList.add("inredient-list");
-
-      let ingredientLines = hits[i].recipe.ingredientLines;
-      // console.log(ingredientLines[0])
-      for (let i = 0; i < ingredientLines.length; i++) {
-        const li = document.createElement("li");
-        li.innerHTML = ingredientLines[i];
-        ul.appendChild(li);
-      }
-      infoDiv.appendChild(ul);
-
-      imageDiv.appendChild(image);
-      imageDiv.appendChild(anchorText);
-      cardDiv.appendChild(imageDiv);
-      infoDiv.appendChild(calorie_span);
-      infoDiv.appendChild(serving_span);
-      infoDiv.appendChild(time_span);
-      cardDiv.appendChild(infoDiv);
-    }
-  });
+  fetchRecipe(foodToSearch).then(renderScreen);
 }
 
+function renderScreen(data) {
+  // creates another function with parameter (data) to run AFTER await fetch is complete on line 13
+  let resultsFound = document.getElementById("resultsFound"); // span created to list result count
+  resultsFound.innerHTML = data.count;
 
+  let hits = data.hits;
+  const recipe_section = document.getElementById("recipe-section");
+  recipe_section.innerHTML = "";
+
+  for (let i = 0; i < 10; i++) {
+    // main div added to body
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("flex-card-div");
+    recipe_section.appendChild(cardDiv);
+
+    //anchor text
+    const anchorText = document.createElement("a");
+    anchorText.id = "recipe-link";
+    anchorText.href = hits[i].recipe.url;
+    anchorText.text = hits[i].recipe.label;
+
+    //image
+    const image = document.createElement("img");
+    image.classList.add("recipe-image");
+    image.src = hits[i].recipe.image;
+
+    // calories
+    const calorie_span = document.createElement("span");
+    calorie_span.classList.add("food-info-span");
+    calorie_span.innerHTML = `Calories: ${Math.floor(hits[i].recipe.calories)}`;
+
+    // serving
+    const serving_span = document.createElement("span");
+    serving_span.classList.add("food-info-span");
+    serving_span.innerHTML = `Serves: ${hits[i].recipe.yield}`;
+    // time
+    const time_span = document.createElement("span");
+    time_span.classList.add("food-info-span");
+    let cookingTime = hits[i].recipe.totalTime
+      ? `${hits[i].recipe.totalTime} mins`
+      : "NA";
+    time_span.innerHTML = `Total Time: ${cookingTime}`;
+    // ingredient list
+    const ul = document.createElement("ul");
+    ul.classList.add("inredient-list");
+    let ingredientLines = hits[i].recipe.ingredientLines;
+    for (let i = 0; i < ingredientLines.length; i++) {
+      const li = document.createElement("li");
+      li.innerHTML = ingredientLines[i];
+      ul.appendChild(li);
+    }
+    // DIV creation and appending
+    const imageDiv = document.createElement("div");
+    imageDiv.classList.add("flex-list-items");
+    imageDiv.appendChild(image);
+    imageDiv.appendChild(anchorText);
+
+    const infoDiv = document.createElement("div");
+    infoDiv.classList.add("food-info-div");
+    infoDiv.appendChild(ul);
+    infoDiv.appendChild(calorie_span);
+    infoDiv.appendChild(serving_span);
+    infoDiv.appendChild(time_span);
+
+    cardDiv.appendChild(imageDiv);
+    cardDiv.appendChild(infoDiv);
+  }
+}
